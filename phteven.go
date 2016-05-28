744c624263fd7371148a77fb95874a3d1614637d
Anh Do <dotuanhoanganh@gmail.com>
2016-05-28 12:20:25 +1000

    Step 4

--- phteven.go
+++ phteven.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	url := "http://api.phteven.io/"
 
-	// Go uses error values to indicate an abnormal state.
-	// Making errors part of the code is clearer than throwing exception.
-	_, err := http.Get("test")
+	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Error sending request: %v\n", err)
 		return
 	}
 
-	fmt.Printf("Our API url is %s\n", url)
+	// Defer is used to ensure that a function call is performed later in a program’s execution, usually for purposes of cleanup. defer is often used where e.g. ensure and finally would be used in other languages.
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error reading body: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Response: %s\n", body)
 }
