c7ce00ef96065027f520bd6ea2578a53a5cf797e
Anh Do <dotuanhoanganh@gmail.com>
2016-05-28 12:25:57 +1000

    Step 5

--- phteven.go
+++ phteven.go
@@ -7,22 +7,26 @@ import (
 )
 
 func main() {
+	body, _ := getPhtevenResponseBody()
+	fmt.Printf("Response: %s\n", body)
+}
+
+func getPhtevenResponseBody() ([]byte, error) {
 	url := "http://api.phteven.io/"
 
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Error sending request: %v\n", err)
-		return
+		return []byte(""), err
 	}
 
-	// Defer is used to ensure that a function call is performed later in a program’s execution, usually for purposes of cleanup. defer is often used where e.g. ensure and finally would be used in other languages.
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("Error reading body: %v\n", err)
-		return
+		return []byte(""), err
 	}
 
-	fmt.Printf("Response: %s\n", body)
+	return body, nil
 }
