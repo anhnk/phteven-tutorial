c895bdca84cc3fcbe835c37c0a7efe48fa97b2c3
Anh Do <dotuanhoanganh@gmail.com>
2016-05-28 12:08:01 +1000

    Step 2

--- phteven.go
+++ phteven.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	// Declare variable
+	url := "http://api.phteven.io/"
+
+	// Package fmt implements formatted I/O with functions analogous to C's printf and scanf. The format 'verbs' are derived from C's but are simpler.
+	fmt.Printf("Our API url is %s\n", url)
 }
